440aa301330e0c0cf2c3413cca310e597162d263
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-11 10:50:35 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const schema = require('./schema/schema.js');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(
-  'graphql',
+  '/graphql',
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development',
